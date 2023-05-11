4acf5673f62defb48ca2e6f20891167bc2427cf6
s0m31 <admin@nwolfhub.org>
2023-05-11 12:20:57 +0300

    Init project

---
+++ src/main/java/org/nwolfhub/Configurator.java
@@ -0,0 +1,13 @@
+package org.nwolfhub;
+
+import org.nwolfhub.easycli.EasyCLI;
+
+import java.util.HashMap;
+
+public class Configurator {
+    private HashMap<String, String> parsed;
+    private String raw;
+    private EasyCLI cli;
+
+
+}
