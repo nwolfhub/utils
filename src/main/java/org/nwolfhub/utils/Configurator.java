5dc8692a83f344c5067fe731607b9facc857bc46
s0m31 <admin@nwolfhub.org>
2023-05-16 20:57:29 +0300

    Fixes #2

--- src/main/java/org/nwolfhub/utils/Configurator.java
+++ src/main/java/org/nwolfhub/utils/Configurator.java
@@ -4,7 +4,9 @@ import org.nwolfhub.easycli.EasyCLI;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 public class Configurator {
@@ -49,6 +51,34 @@ public class Configurator {
         readConfig();
     }
 
+    public void createConfigIfNotExists() throws IOException {
+        if(!this.configFile.exists()) {
+            configFile.createNewFile();
+        }
+    }
+    public void createConfigIfNotExists(String content) throws IOException {
+        if(!this.configFile.exists()) {
+            configFile.createNewFile();
+            try (FileOutputStream outputStream = new FileOutputStream(configFile)) {
+                outputStream.write(content.getBytes(StandardCharsets.UTF_8));
+            }
+        }
+    }
+
+    public boolean createConfig() throws IOException {
+        if(!this.configFile.exists()) {
+            createConfigIfNotExists();
+            return false;
+        }
+        return true;
+    }
+    public boolean createConfig(String content) throws IOException {
+        if(!this.configFile.exists()) {
+            createConfigIfNotExists(content);
+            return false;
+        }
+        return true;
+    }
     private void readConfig() {
         try(FileInputStream in = new FileInputStream(configFile)) {
             raw = new String(in.readAllBytes());
